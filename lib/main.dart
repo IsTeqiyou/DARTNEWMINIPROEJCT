acbba6a748b6cab7bd65891060b603e414d1abeb
IsTeqiyou <taqiyuddin2011@student.abudzar.sch.id>
2026-04-17 09:28:31 +0700

    TUGAS PROJECT LAST

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+class HttpApi extends StatefulWidget {
+  const HttpApi({super.key});
+
+  @override
+  State<HttpApi> createState() => _HttpApiState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _HttpApiState extends State<HttpApi> {
+  List data = [];
+  bool isLoading = true;
+
+  Future ambilData() async {
+    var response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+
+    var hasil = jsonDecode(response.body);
+
+    setState(() {
+      data = hasil;
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    ambilData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Slicing UI App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'sans-serif',
-      ),
-      home: const SplashScreen(),
+    return Scaffold(
+      appBar: AppBar(title: Text("Latihan Ambil DATA")),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView(
+              children: data.map((item) {
+                return ListTile(title: Text(item['title']));
+              }).toList(),
+            ),
     );
   }
 }
